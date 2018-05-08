b686f9cdde9ac9248f646b3ba73105b0b20ecc31
NStephenson <nickstephenson90@gmail.com>
2018-05-08 17:08:20 -0400

    added mock homepage data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {AboutMe} from './components/aboutMe'
+import { Resume } from './components/resume'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hi Michelle! Love ya!</h1>
+          <h1>Rubber Kalimba</h1>
+          <p> The home of Nicholas Stephenson</p>
         </header>
-        <p className="App-intro">
-        </p>
+        <h1 class="home-card">About Me</h1>
+        <h1 class="home-card">Projects</h1>
+        <h1 class="home-card">Resume</h1>
+        <h1 class="home-card">Contact</h1>
+        <Resume/>
       </div>
     );
   }
