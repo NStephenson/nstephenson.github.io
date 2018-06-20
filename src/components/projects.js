0709d039ad411de0ee39b41f004d3058fe7685ef
NStephenson <nickstephenson90@gmail.com>
2018-06-20 14:57:59 -0400

    changed project page to compenent with state, created basic layout

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,14 +1,45 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Project} from './project'
 import {projectData} from '../data'
 
-export const Projects = () => {
+export class Projects extends Component {
 
-  const projectList = projectData.map((d) => <Project {...d}/>)
+  constructor(props){
+    super(props)
+    this.state = {index: 0}
+    this.handlePrev = this.prevClick.bind(this)
+    this.handleNext = this.nextClick.bind(this)
+  }
 
-  return (
-    <div>
-      {projectList}
+  prevClick(){
+    if(this.state.index === 0){
+      this.setState({index: projectData.length - 1})
+    } else {
+      this.setState({index: --this.state.index})
+    }
+  }
+
+  nextClick(){
+    if(this.state.index === projectData.length - 1){
+      this.setState({index: 0})
+    } else {
+      this.setState({index: ++this.state.index})
+    }
+  }
+
+  
+  render(){
+    // if(!this.state){
+    //   return ("Loading")
+    // }
+    return (
+      <div className="row projects">
+      <div className="col-md-2" onClick={this.handlePrev}>Prev</div>
+      <div className="col-md-8">
+        <Project {...projectData[this.state.index]}/>
+      </div>
+      <div className="col-md-2" onClick={this.handleNext}>Next</div>
     </div>
-  )
+    )
+  }
 }
