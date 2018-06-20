caea76285b8f293ce735573362050616b69aa4da
NStephenson <nickstephenson90@gmail.com>
2018-06-20 14:53:19 -0400

    about me now uses imported data

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -1,14 +1,14 @@
 import React from 'react'
+import {aboutMe} from '../data'
 
 export const AboutMe =  () => {
  return(
-    <div className="row">
-      <p className="col-md-6">
-      I am Nicholas Stephenson. I am a Technical Coach at The Flatiron School where I 
-      provide genral lab support as well as 1 on 1 Portfolio Project supporrt. Aside from development
-      I'm passionate about the entertainment industry, health, and assisting 
-      vulenerable populations via volunteer work. 
+    <div className="row middle" id="about-me">
+
+      <p className="col-md-8">
+        {aboutMe}
       </p>
+
     </div>
   )
 }
