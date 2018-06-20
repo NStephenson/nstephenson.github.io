cc8d95e747f73d3192c5111b86ad69603eae4e1f
NStephenson <nickstephenson90@gmail.com>
2018-06-20 14:57:11 -0400

    minor fixes and changed link to open in new tab

--- src/components/project.js
+++ src/components/project.js
@@ -1,16 +1,15 @@
 import React from 'react'
 
-export const Project = ({title, link, image, github, description}) => {
+export const Project = ({name, link, image, github, description}) => {
   return (
-    <div className="project">
-    <h2>{title}</h2>
-    {/* List of technologies*/}
-    <a href="{link}">
-      <img src="{image}" alt="" className="project-image"/>
-    </a>
-    <p>{description}</p>
-    <a href="{github}">Github Repo</a>
-
-    </div>
+      <div className="project">
+        <h2>{name}</h2>
+        {/* List of technologies*/}
+        <a href={link} target="_blank">
+          <div className="test"><img src={image} alt="" className="project-image mx-auto"/></div>
+        </a>
+        <p>{description}</p>
+        <a href="{github}">Github Repo</a>
+      </div>
   )
 }
