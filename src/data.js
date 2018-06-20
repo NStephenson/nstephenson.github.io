175630b4323ad0833faacdbb8e9364afdd044554
NStephenson <nickstephenson90@gmail.com>
2018-06-20 14:50:17 -0400

    changed dummy datat to actual data

--- src/data.js
+++ src/data.js
@@ -2,31 +2,27 @@
 
 //// PROJECTS PAGE
 
-class Project {
-  constructor(name){
-
-  }
-}
+export const aboutMe = `My name is Nicholas Stephenson. On top of being a Web Developer, I’m a lifelong music enthusiast, a film nerd, and I’m passionate about support vulnerable populations in my community via volunteer work. The technologies I currently utilize are Rails 5, Angular,  React, Bootstrap 4, PostgreSQL, Git, Ruby, and Typescript, but I’m always adding more to my skillset or honing the effectiveness of my current tools. Prior to my experience in Development, I’ve covered a variety of roles in the entertainment industry where I’ve been constantly challenged to quickly adapt to new experiences and teams of people and I always look forward to taking on new challenges.`
 
 export const projectData = [
   {
     name: "Quick Poll",
     link: 'https://smallpoll.herokuapp.com/',
-    image: '',
+    image: 'project_images/quick_poll.png',
     github: 'https://github.com/NStephenson/quick-poll-fe',
     description: 'A simple tool to create/manage polls and share them with friends'
   },
   {
     name: "LE3",
     link: 'https://youtube.com/',
-    image: '',
+    image: 'project_images/LE3.png',
     github: 'https://github.com/NStephenson/LE3',
     description: 'A chrome extension designed to enhance user experience of the Technical Coach platform'
   },
   {
     name: "Hot 100 CLI",
     link: 'https://rubygems.org/gems/Hot_100_CLI',
-    image: '',
+    image: 'project_images/hot100.png',
     github: 'https://github.com/NStephenson/Hot_100_CLI',
     description: 'An interactive, intelligent command line version of The Billboard’s Hot 100 Singles Chart for music enthusiasts.'
   }
@@ -34,9 +30,8 @@ export const projectData = [
 
 
 export const contactData = [
-  {text: "github", link: 'www.github.com/nstpehenson', logo: ''},
-  {text: 'email', link: 'nickstephenson90@gmail.com', logo: ''},
-  {text: 'blog', link: 'https://ispeakmachine.wordpress.com', logo: ''},
-  {text: 'linkedIn', link: '', logo: ''}
+  {text: "github", link: 'https://www.github.com/nstephenson', logo: 'social_icons/github-sign.svg'},
+  {text: 'blog', link: 'https://ispeakmachine.wordpress.com', logo: 'social_icons/wordpress-logo.svg'},
+  {text: 'linkedIn', link: 'https://www.linkedin.com', logo: 'social_icons/linkedin-logo.svg'}
 
 ]
