0e1ce619158d3c781d580a99c28fce21ff845bba
NStephenson <nickstephenson90@gmail.com>
2018-06-20 14:51:04 -0400

    added iframe to row div

--- src/components/resume.js
+++ src/components/resume.js
@@ -3,10 +3,8 @@ import React from 'react';
 export const Resume = () => {
   return (
 
-    <div>
-      
-      <iframe src="https://drive.google.com/file/d/1WmDNIFZHEFLTYx3x_7Uury5i49VOQ3Jz/preview" width="640" height="480"></iframe>
-
+    <div className="row">
+      <iframe className="model" src="https://drive.google.com/file/d/1WmDNIFZHEFLTYx3x_7Uury5i49VOQ3Jz/preview" frameborder="0" allowfullscreen=""></iframe>
     </div>
 
   )
