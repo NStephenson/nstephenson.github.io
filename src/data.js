d267027d588238a2f09c15752b376fd16ea37df7
NStephenson <nickstephenson90@gmail.com>
2018-06-25 12:06:26 -0400

    removed public folder from routes

--- src/data.js
+++ src/data.js
@@ -2,27 +2,27 @@
 
 //// PROJECTS PAGE
 
-export const aboutMe = `My name is Nicholas Stephenson. On top of being a Web Developer, I’m a lifelong music enthusiast, a film nerd, and I’m passionate about support vulnerable populations in my community via volunteer work. The technologies I currently utilize are Rails 5, Angular,  React, Bootstrap 4, PostgreSQL, Git, Ruby, and Typescript, but I’m always adding more to my skillset or honing the effectiveness of my current tools. Prior to my experience in Development, I’ve covered a variety of roles in the entertainment industry where I’ve been constantly challenged to quickly adapt to new experiences and teams of people and I always look forward to taking on new challenges.`
+export const aboutMe = `My name is Nicholas Stephenson. On top of being a Web Developer, I’m a lifelong music enthusiast, a film nerd, and I’m passionate about supporting vulnerable populations in my community via volunteer work. The technologies I currently utilize are Rails 5, Angular,  React, Bootstrap 4, PostgreSQL, Git, Ruby, and Typescript, but I’m always adding more to my skillset or honing the effectiveness of my current tools. Prior to my experience in development, I’ve covered a variety of roles in the entertainment industry where I’ve been constantly challenged to quickly adapt to new experiences and teams of people and I always look forward to taking on new challenges.`
 
 export const projectData = [
   {
     name: "Quick Poll",
     link: 'https://smallpoll.herokuapp.com/',
-    image: 'public/project_images/quick_poll.png',
+    image: 'project_images/quick_poll.png',
     github: 'https://github.com/NStephenson/quick-poll-fe',
     description: 'A simple tool to create/manage polls and share them with friends'
   },
   {
     name: "LE3",
     link: 'https://youtube.com/',
-    image: 'public/project_images/LE3.png',
+    image: 'project_images/LE3.png',
     github: 'https://github.com/NStephenson/LE3',
     description: 'A chrome extension designed to enhance user experience of the Technical Coach platform'
   },
   {
     name: "Hot 100 CLI",
     link: 'https://rubygems.org/gems/Hot_100_CLI',
-    image: 'public/project_images/hot100.png',
+    image: 'project_images/hot100.png',
     github: 'https://github.com/NStephenson/Hot_100_CLI',
     description: 'An interactive, intelligent command line version of The Billboard’s Hot 100 Singles Chart for music enthusiasts.'
   }
@@ -30,8 +30,8 @@ export const projectData = [
 
 
 export const contactData = [
-  {text: "github", link: 'https://www.github.com/nstephenson', logo: 'public/social_icons/github-sign.svg'},
-  {text: 'blog', link: 'https://ispeakmachine.wordpress.com', logo: 'public/social_icons/wordpress-logo.svg'},
-  {text: 'linkedIn', link: 'https://www.linkedin.com', logo: 'public/social_icons/linkedin-logo.svg'}
+  {text: "github", link: 'https://www.github.com/nstephenson', logo: 'social_icons/github-sign.svg'},
+  {text: 'blog', link: 'https://ispeakmachine.wordpress.com', logo: 'social_icons/wordpress-logo.svg'},
+  {text: 'linkedIn', link: 'https://www.linkedin.com/in/nicholas-stephenson-a779a215/', logo: 'social_icons/linkedin-logo.svg'}
 
 ]
