0b9501536b8c7fc46a7b160868807aa9959d8b9c
NStephenson <nickstephenson90@gmail.com>
2018-06-25 12:08:57 -0400

    added public url to images

--- src/components/contactBubble.js
+++ src/components/contactBubble.js
@@ -5,7 +5,7 @@ export const ContactBubble = ({link, text, logo}) => {
   return (
     <div className="contact-bubble ">
       <a href={link} target='_blank'>
-        <img src={logo} alt={text + '-logo'} className="social-icon"/>
+        <img src={process.env.PUBLIC_URL + logo} alt={text + '-logo'} className="social-icon"/>
       </a>
     </div>
   )
--- src/components/project.js
+++ src/components/project.js
@@ -5,11 +5,11 @@ export const Project = ({name, link, image, github, description}) => {
       <div className="project">
         <h2>{name}</h2>
         {/* List of technologies*/}
-        <a href={link} target="_blank">
+        <a href={process.env.PUBLIC_URL + link} target="_blank">
           <div className="test"><img src={image} alt="" className="project-image mx-auto"/></div>
         </a>
         <p>{description}</p>
-        <a href="{github}">Github Repo</a>
+        <a href={github} target="_blank">Github Repo</a>
       </div>
   )
 }
