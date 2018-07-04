cab384fffe97bfd3db9e9d42b0ce372050e91591
NStephenson <nickstephenson90@gmail.com>
2018-07-04 18:13:45 -0400

    started changes to improve responsiveness

--- src/App.js
+++ src/App.js
@@ -15,38 +15,39 @@ class App extends Component {
 
       {/* This is to create a border  */}
       <div id="left"></div>
-      <div id="right"></div>
+      <div id="right"></div> 
       <div id="top" className="row"></div>
       <div id="bottom" className="row"></div>
       <Router>
-      <div className="container site-content">
-        <header className="App-header row ">
-          <nav className="navbar navbar-expand-md">
-            <Link to="/" className="nav-item nav-link">Home</Link>
-            <Link to="/about" className="nav-item nav-link">About Me</Link>
-            <Link to="/projects" className="nav-item nav-link">Projects</Link>
-            <Link to="/resume" className="nav-item nav-link">Resume</Link>
-            <Link to="/contact" className="nav-item nav-link">Contact</Link>
-          </nav>
-          
-        </header>
-        <div className="main align-middle">
+      <div className="container-fluid site-content row">
+          <header className="App-header col-12">
+            <nav className="navbar navbar-expand-md">
+              <Link to="/" className="nav-item nav-link">Home</Link>
+              <Link to="/about" className="nav-item nav-link">About Me</Link>
+              <Link to="/projects" className="nav-item nav-link">Projects</Link>
+              <Link to="/resume" className="nav-item nav-link">Resume</Link>
+              <Link to="/contact" className="nav-item nav-link">Contact</Link>
+            </nav>
+            
+          </header>
+        <div className="main col-12">
         
-        <div>
+
           <Route exact path="/" component= { Homepage }/>
           <Route path="/about"  component= { AboutMe }/>
           <Route path="/projects"  component= { Projects }/>
           <Route path="/resume"  component= { Resume }/>
           <Route path="/contact"  component= { ContactPage }/>
-          </div>
-        
+
+        <div className="row">
+          <footer className="footer col-12">Copyright © 2018 - Nicholas Stephenson - <a href="https://github.com/NStephenson/nstephenson.github.io">Source Code</a></footer>
+        </div>
         
         </div>
       
         </div>
       </Router>
-          <footer className="footer">Copyright © 2018 - Nicholas Stephenson - <a href="https://github.com/NStephenson/nstephenson.github.io">Source Code</a></footer>
-
+        
       </div>
     );
   }
--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -3,9 +3,9 @@ import {aboutMe} from '../data'
 
 export const AboutMe =  () => {
  return(
-    <div className="row middle" id="about-me">
+    <div className="row align-items-center" id="about-me">
 
-      <p className="col-md-8">
+      <p className="col-sm-6 offset-sm-3">
         {aboutMe}
       </p>
 
