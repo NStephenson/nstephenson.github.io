589959e65427ac4d175ac5e7ff592b98311de999
Nick Stephenson <nickstephenson90@gmail.com>
2018-07-06 16:38:46 -0400

    fixed names of attributes

--- src/components/resume.js
+++ src/components/resume.js
@@ -4,7 +4,7 @@ export const Resume = () => {
   return (
 
     <div className="row">
-      <iframe className="model" src="https://drive.google.com/file/d/1WmDNIFZHEFLTYx3x_7Uury5i49VOQ3Jz/preview" frameborder="0" allowfullscreen=""></iframe>
+      <iframe className="model" src="https://drive.google.com/file/d/1WmDNIFZHEFLTYx3x_7Uury5i49VOQ3Jz/preview" frameBorder="0" allowFullScreen=""></iframe>
     </div>
 
   )
