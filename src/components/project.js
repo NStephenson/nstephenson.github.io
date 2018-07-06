32ad0c320b413fda01cb9bfe2f4abefd45f41a2f
Nick Stephenson <nickstephenson90@gmail.com>
2018-07-06 17:01:02 -0400

    changed background image css to be inline

--- src/components/project.js
+++ src/components/project.js
@@ -6,7 +6,7 @@ export class Project extends Component {
   render(){
     let props = this.props
     return (
-      <div className="project col-md-3" id={'project-' + props.id}>
+      <div className="project col-md-3" id={'project-' + props.id} style={{backgroundImage: `url(${process.env.PUBLIC_URL + props.image}`}}>
         <figcaption className="invis">
           <a href={props.link} target="_blank">
             <h2>{props.name}</h2>
