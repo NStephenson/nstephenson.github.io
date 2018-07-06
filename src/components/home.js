58412b9ae3b3063ecb4ef48d3b6dd0798b32acd2
Nick Stephenson <nickstephenson90@gmail.com>
2018-07-06 16:40:45 -0400

    changed offset of title

--- src/components/home.js
+++ src/components/home.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Homepage = () => {
   return (
     <div className="row middle" id="homepage">
-    <div className="col-sm-6 offset-sm-3">
+    <div className="col-sm-6 offset-sm-4 align-top">
       <h1>Rubber Kalimba</h1>
       <p> (The home of Nicholas Stephenson)</p>
     </div>
