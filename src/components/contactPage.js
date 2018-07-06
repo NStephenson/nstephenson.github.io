72d6b54e1f43275834390fcb2e4977c435003ecf
Nick Stephenson <nickstephenson90@gmail.com>
2018-07-06 16:42:43 -0400

    changed formatting

--- src/components/contactPage.js
+++ src/components/contactPage.js
@@ -6,15 +6,16 @@ export const ContactPage = () => {
   const contactBubbles = contactData.map((t) => <ContactBubble {...t}/>)
 
   return (
-    <div className="middle">
-      <div className='' id="contact-bubbles">
+    <div className="middle" id="contact-page">
+    <div className="row">
+      <div className='col-4 offset-4' id="contact-bubbles">
         {contactBubbles}
       </div>
-      <div className="row">
-        <p className='col-12'>Or send me an email at: <a target="_blank" href="mailto:nick@rubberkalimba.com">nick@rubberkalimba.com</a></p>
-      </div>
-      
     </div>
+      <div className="row">
+        <p className='col-6 offset-3'>Or send me an email at: <a target="_blank" href="mailto:nick@rubberkalimba.com">nick@rubberkalimba.com</a></p>
+      </div>
+    </div>  
   )
 }
 
